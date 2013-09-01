78643e657f98cc3b11d774c1240eeb499a7b9bb7
diegoram <ddarkheart@gmail.com>
2013-09-01 17:00:16 -0300

    fix the move demo using the proper values table

--- gpiohard.py
+++ gpiohard.py
@@ -1,43 +1,61 @@
 import RPi.GPIO as gpio
 from time import sleep 
 
+
 gpio.cleanup()
 
 gpio.setmode(gpio.BOARD)
 
 
 ##Left Side
-gpio.setup(11,gpio.OUT)
-gpio.setup(13,gpio.OUT)
-
+gpio.setup(19,gpio.OUT)
+gpio.setup(21,gpio.OUT)
+gpio.output(19,True)
+gpio.output(21,True)
 ##Right Side
-gpio.setup(15,gpio.OUT)
-gpio.setup(16,gpio.OUT)
+gpio.setup(22,gpio.OUT)
+gpio.setup(24,gpio.OUT)
+gpio.output(22,True)
+gpio.output(24,True)
 
 #PWM
 gpio.setup(12,gpio.OUT)
+gpio.setup(18,gpio.OUT)
 
-pwm = gpio.PWM(12, 1500)
+pwm_left = gpio.PWM(12, 1500)
+pwm_right = gpio.PWM(18, 1500)
+
+def move(arc):
+	pwm_left.start(0)
+	pwm_right.start(0)
+	cycle_left, cycle_right = arc
+	try:  
+	    while True:  
+		pwm_left.ChangeDutyCycle(cycle_left)
+		pwm_right.ChangeDutyCycle(cycle_right)
+	except KeyboardInterrupt:  
+	    pwm_left.stop()
+	    pwm_right.stop()
+
+
 
 ##Forward
-gpio.output(11, True)
-gpio.outout(15, True)
-pwm.start(0)
-try:  
-    while True:  
-		pwm.ChangeDutyCycle(50)
-except KeyboardInterrupt:  
-    pwm.stop()
+#21 on
+gpio.output(19, False)
+#24 on
+gpio.output(22, False)
 
+move((40,100))
 
 ##barckward
-gpio.output(13, True)
-gpio.outout(16, True)
-pwm.start(0)
-try:  
-    while True:  
-		pwm.ChangeDutyCycle(90)
-except KeyboardInterrupt:  
-    pwm.stop()
+gpio.output(21, False)
+gpio.output(19, True)
+
+gpio.output(24, False)
+gpio.output(22, True)
+
+move((100,40))
+
+
 
 gpio.cleanup()         
