530733534fc999ddd18457e51b525a53d31bdbc6
Fernando Claverino <fernando.claverino@gmail.com>
2013-10-26 11:44:43 -0300

    nivel 3

---
+++ nivel3.py
@@ -0,0 +1,5 @@
+from nivel3_api import *
+
+for numero in range(1,5):
+	adelante(numero)
+	girar_izquierda(numero)
---
+++ nivel3_api.py
@@ -0,0 +1,28 @@
+from time import sleep
+from braianDriver.robot import Robot
+
+robot = Robot()
+
+def adelante(cuanto):
+	robot.set_forward()
+	robot.move(speed=Robot.SPEED_MEDIUM)
+	sleep(cuanto)
+	robot.stop()
+
+def atras(cuanto):
+	robot.set_backward()
+	robot.move(speed=Robot.SPEED_HIGH)
+	sleep(cuanto)
+	robot.stop()
+
+def girar_izquierda(cuanto):
+	robot.set_rotate_left()
+	robot.move(speed=Robot.SPEED_LOW)
+	sleep(cuanto)
+	robot.stop()
+
+def girar_derecha(cuanto):
+	robot.set_rotate_right()
+	robot.move(speed=Robot.SPEED_LOW)
+	sleep(cuanto)
+	robot.stop()
