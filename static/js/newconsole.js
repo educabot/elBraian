7334c3e7c213978f2bd1d98ba7d9a8e40d8a2824
diegoram <ddarkheart@gmail.com>
2014-06-07 19:43:56 -0300

    add js behaviour

---
+++ static/js/newconsole.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	
+
+	//Just for testing
+	$('#sort1, #sort2' ).sortable({
+		connectWith: ".list-group",
+		placeholder: "list-group-item.active"
+		}).disableSelection();
+
+	$('#actions li').draggable({
+		appendTo: "body",
+		helper: "clone"
+	});
+
+	$("#steps").droppable({
+		activeClass: "ui-state-default",
+      	hoverClass: "ui-state-hover",
+      	accept: ":not(.ui-sortable-helper)",
+		drop: function(event, ui){
+			$(this).find('#placeholder').remove();
+			var text = ui.draggable.text();
+			$('<li class="list-group-item"></li>').appendTo(this);
+			if(text.indexOf("avanzar")> -1){
+				$('<span class="glyphicon glyphicon-arrow-up"></span>').text( ui.draggable.text()).appendTo($('#steps li:last-child'));
+			} else if (text.indexOf("parar")> -1){
+				$('<span class="glyphicon glyphicon-stop"></span>').text( ui.draggable.text()).appendTo($('#steps li:last-child'));
+			} else if (text.indexOf("retroceder")> -1){
+				$('<span class="glyphicon glyphicon-arrow-down"></span>').text( ui.draggable.text()).appendTo($('#steps li:last-child'));
+			} else if (text.indexOf("girar derecha")> -1){
+				$('<span class="glyphicon glyphicon-chevron-right"></span>').text( ui.draggable.text()).appendTo($('#steps li:last-child'));
+			} else if (text.indexOf("girar izquierda")> -1){
+				$('<span class="glyphicon glyphicon-chevron-left"></span>').text( ui.draggable.text()).appendTo($('#steps li:last-child'));
+			}
+
+		}
+	}).sortable({
+		items: "li:not(.placeholder)",
+		sort: function() {
+	        $( this ).removeClass( "ui-state-default" );
+      }
+	});
+});
