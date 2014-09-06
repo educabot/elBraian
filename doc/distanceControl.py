e491a84ff33a0baf2bf3295561be8cf5be16f00c
diegoram <ddarkheart@gmail.com>
2014-09-06 14:26:19 -0300

    add movement to distance sensor PoC

--- doc/distanceControl.py
+++ doc/distanceControl.py
@@ -1,13 +1,26 @@
+from braianDriver.robot import Robot 
 import RPIO as rpio 
 import time 
 steps = 0
 start = 0
 stop = 0
+robot = Robot()
+counting_right = 0
+counting_left = 0
 
 def gpio_callback(gpio_id, val):
-	global steps, stop
+	global steps, stop, robot, counting_right, counting_left
 	steps += 1
+	
+	if gpio_id == 4:
+		counting_left += 1
+	elif gpio_id == 21:
+		counting_right += 1
+
+	if counting_right - counting_left > 2 :
+		
 	if(steps >= int(input)):
+		robot.stop()
 		rpio.stop_waiting_for_interrupts()
 		stop = int(round(time.time) * 1000)
     print("gpio %s: %s, step counting: %s" % (gpio_id, val, steps) )
@@ -19,7 +32,10 @@ start = int(round(time.time) * 1000)
 
 input = raw_input("ingrese numero de pasos")
 
-rpio.wait_for_interrupts()
+robot.set_forward()
+robot.move(speed = Robot.SPEED_HIGH)
+robot.move(speed = Robot.SPEED_MEDIUM)
+rpio.wait_for_interrupts(threaded = True)
 
 velCms = (1.4 * steps) / (stop - start)
 
