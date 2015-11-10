f711b7e04f9d964ad2f2a133a4a6c76e272b5e44
diegoram <diego@diegoram.me>
2015-11-09 22:26:37 -0300

    fixed common.js file

--- static/js/common.js
+++ static/js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var lastkey;
-	//var host = "ws://"+ window.location.hostname +"/robot";
+	var host = "ws://"+ window.location.hostname +"/robot";
 
 	console.log("url socket: " + document.domain);
 
