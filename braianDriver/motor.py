1ca576a03fb50988539e5978f572927d56baa760
diegoram <diego@diegoram.me>
2016-02-10 09:07:15 -0300

    motor class

--- braianDriver/motor.py
+++ braianDriver/motor.py
@@ -1,11 +1,31 @@
-class Motor:
+class Motor(object):
+    def __init__(gpio,pin_set_left, pin_set_right):
+    """
+    Motor constructor
+    Args:
+        pin_set_left: tuple to represent pins to control left wheel
+        pin_set_right: idem the later to control right wheel
+    Returns:
+        Proper Motor Object
+    """
+        self.gpio = gpio
+        self.pin_set_left = pin_set_left
+        self.pin_set_right = pin_set_right
+
     def move_forward(self):
+        """
+        Set pins to move motor and wheel forward
+        """
         pass
 
     def move_backward(self):
+        """
+        set pins to move motor and whell backward
+        """
         pass
 
 
 class MotorDC(Motor):
+
 
 class Stepper(Motor):
